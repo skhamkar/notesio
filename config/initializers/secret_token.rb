33388b66754f08ad08156a79300749647f39cd1c
susheel <susheelkhamkar2@gmail.com>
2012-12-01 14:34:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notesio::Application.config.secret_token = '28af5ae294714f12cab113f4fe8893f3214edd16c1f494abc2fa723f97da3b248ab7edc14d5e099fd95ab158c6eb89002718a156891e7b66443484222db0778d'
